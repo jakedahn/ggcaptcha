b865a40896025845ba52e5f896afa8696609e5a8
Jake Dahn <admin@jakedahn.com>
2011-06-19 22:24:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captcha::Application.config.secret_token = 'ca6525ef2444b010729a34ddf37cea749e7c53dece1e9f12acfd82083f5be6723d4e17f3c7452ca82214cefd6e6f84ba4cfc6919f46da7a47a489c48330695a4'
